91f27c129e750fdab5c6381618df58763c23e229
Carlos-Salmentro <carlos.salmentro@gmail.com>
2023-02-11 10:32:08 -0300

    Bee2152

---
+++ Bee2152/Bee2152/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string Status;
+        int X = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < X; i++)
+        {
+
+            string data = Console.ReadLine();
+            string[] array = data.Split(' ');
+
+            string H = array[0].PadLeft(2, '0');
+            string M = array[1].PadLeft(2, '0');
+            int O = int.Parse(array[2]);
+
+            if (O == 0)
+            {
+                Status = "A porta fechou!";
+            }
+
+            else
+            {
+                Status = "A porta abriu!";
+            }
+
+            Console.WriteLine($"{H}:{M} - {Status}");
+
+        }
+    }
+}
