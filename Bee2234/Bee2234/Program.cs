0e180c2b98838be3a1393a4fd575715a4ea1bd52
CarlosSalmentro <carlos.salmentro@gmail.com>
2023-02-12 22:55:52 -0300

    Add - BeeCrowd 2152 & 2234

---
+++ Bee2234/Bee2234/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+public class Program
+{
+    public static void Main()
+    {
+        string Data = Console.ReadLine();
+
+        string[] Infs = Data.Split(' ');
+
+        double H = double.Parse(Infs[0]);
+        double P = double.Parse(Infs[1]);
+
+        double X = Math.Round(H / P, 2); 
+
+        Console.WriteLine(X.ToString("F2", CultureInfo.InvariantCulture));
+    }
+}
