e0ff77ab2f9c94dea187746a178b5ac84a64fbdd
CarlosSalmentro <carlos.salmentro@gmail.com>
2023-02-13 23:58:15 -0300

    Add Bee1021

---
+++ Bee2116/Bee2116/Bee2116/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        string[] Array = Console.ReadLine().Split(' ');
+
+        double N = double.Parse(Array[0]);
+        double M = double.Parse(Array[1]);
+
+        int P1 = 0;
+        int P2 = 0;
+
+        int count = 0;
+
+        if (N == 2)
+        {
+            P1 = 2;
+        }
+
+        else
+        {
+            for (int i = 2; i <= N && count == 0; i++)
+            {
+                double x = Convert.ToDouble(i);
+
+                if (N % x == 0 && i != N)
+                {
+                    i = 1;
+                    N--;
+                }
+                if (i == N)
+                {
+                    P1 = Convert.ToInt32(N);
+                    count++;
+                }
+            }
+        }
+
+        count = 0;
+
+        if (M == 2)
+        {
+            P2 = 2;
+        }
+
+        else
+        {
+            for (int i = 2; i <= M && count == 0; i++)
+            {
+                double x = Convert.ToDouble(i);
+
+                if (M % x == 0 && i != M)
+                {
+                    i = 1;
+                    M--;
+                }
+                if (i == M)
+                {
+                    P2 = Convert.ToInt32(M);
+                    count++;
+                }
+            }
+        }
+
+
+        Console.WriteLine(P1 * P2);
+    }
+
+}
