87addad0d2cd2a0b0ab333a4d7f4712a159092a4
CarlosSalmentro <carlos.salmentro@gmail.com>
2023-02-13 22:14:37 -0300

    Add BeeCrowd1960Reverse

---
+++ Bee1960Reverse/Bee1960Reverse/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string RomanNumber = Console.ReadLine();
+        int Size = RomanNumber.Length;
+        char[] RomanLetter = new char[Size];
+        RomanLetter = RomanNumber.ToCharArray();
+        int[] Numbers = new int[Size];
+
+        int Subtract = 0;
+        int Sum = 0;
+        int Result = 0;
+
+        for (int i = 0; i < RomanNumber.Length; i++)
+        {
+            if (RomanLetter[i] == 'I')
+            {
+                Numbers[i] = 1;
+            }
+            else if (RomanLetter[i] == 'V')
+            {
+                Numbers[i] = 5;
+            }
+            else if (RomanLetter[i] == 'X')
+            {
+                Numbers[i] = 10;
+            }
+            else if (RomanLetter[i] == 'L')
+            {
+                Numbers[i] = 50;
+            }
+            else if (RomanLetter[i] == 'C')
+            {
+                Numbers[i] = 100;
+            }
+            else if (RomanLetter[i] == 'D')
+            {
+                Numbers[i] = 500;
+            }
+            else
+            {
+                Numbers[i] = 1000;
+            }
+
+        }
+
+        bool Status = false;
+
+        for (int i = 0; i < Size; i++)
+        {
+            for (int y = i + 1; y < Size && Status == false; y++)
+            {
+                if (Numbers[i] < Numbers[y])
+                {
+                    Subtract += Numbers[i];
+                    Status = true;
+                }
+            }
+
+            if (Status == false)
+            {
+                Sum += Numbers[i];
+            }
+
+            Status = false;
+        }
+
+        Result = Sum - Subtract;
+        Console.WriteLine(Result.ToString());
+
+
+    }
+
+}
---
+++ Bee1960Reverse/README-Challenge.txt
@@ -0,0 +1,23 @@
+beecrowd | 1960
+Numeração Romana para Números de Página
+Por M.C. Pinto, UNILA BR Brazil
+
+Timelimit: 1
+A ECI (Editio Chronica Incredibilis ou Editora de Crônicas Incríveis) é muito tradicional quando se trata de numerar as páginas de seus livros. Ela sempre usa a numeração romana para isso. E seus livros nunca ultrapassam as 999 páginas pois, quando necessário, dividem o livro em volumes.
+
+Você deve escrever um programa que, dado um número arábico, mostra seu equivalente na numeração romana.
+
+Lembre que I representa 1, V é 5, X é 10, L é 50, C é 100, D é 500 e M representa 1000.
+
+Entrada
+A entrada é um número Romano N (0 < N < 1000).
+
+Saída
+A saída é o número N escrito como Inteiro em uma única linha.
+
+Exemplos de Entrada	Exemplos de Saída
+DCLXVI			666
+
+LXXXIII			83
+
+CMXCIX			999
