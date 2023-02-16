0196007870dee76147954ed1f1e9e5fe02ac5316
CarlosSalmentro <carlos.salmentro@gmail.com>
2023-02-16 10:59:11 -0300

    Add Bee1984

---
+++ Bee1984/Bee1984/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+class URI
+{
+    static void Main(string[] args)
+    {
+        string Data = Console.ReadLine();
+        int X = Data.Length - 1;
+        StringBuilder Sb = new StringBuilder();
+
+        for (int i = 0; i <= X; i++)
+        {
+            Sb.Append(Data[X - i]);
+        }
+
+        Console.WriteLine(Sb.ToString());
+    }
+
+}
