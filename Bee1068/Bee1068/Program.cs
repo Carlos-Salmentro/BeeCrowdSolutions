2e7af9161a14ac6ea983ca3519a22deaf132ff22
CarlosSalmentro <carlos.salmentro@gmail.com>
2023-03-03 11:05:21 -0300

    Add - Bee1068

---
+++ Bee1068/Bee1068/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            string line = Console.ReadLine();
+
+            int Open = 0;
+            int Close = 0;
+            int Test = 0;
+
+            if (line == null || line == String.Empty)
+            {
+                break;
+            }
+
+            for (int i = 0; i < line.Length && Test >= 0; i++)
+            {
+                if (line[i] == '(')
+                {
+                    Open++;
+                    Test++;
+                }
+                else if (line[i] == ')')
+                {
+                    Close++;
+                    Test--;
+                }
+            }
+
+            if (Open == Close)
+            {
+                Console.WriteLine("correct");
+            }
+            else
+            {
+                Console.WriteLine("incorrect");
+            }
+
+        }
+
+    }
+
+
+}
+
+
