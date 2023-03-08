c646c94ee14f5fdf3b0f8cc3042c42c31a3779a2
CarlosSalmentro <carlos.salmentro@gmail.com>
2023-03-07 22:27:31 -0300

    Add - Bee1983

---
+++ Bee1983/Bee1983/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+
+        int N = int.Parse(Console.ReadLine());
+        List<Student> students = new List<Student>();
+        Student Choosen = null;
+
+        for (int i = 0; i < N; i++)
+        {
+            string[] Data = new string[2];
+
+            Data = Console.ReadLine().Split(' ');
+
+            int id = int.Parse(Data[0]);
+            double note = Convert.ToDouble((Data[1]), CultureInfo.InvariantCulture);
+
+            Student student1 = new Student(id, note);
+
+            students.Add(student1);
+        }
+
+        List<Student> StudentsOrdered = new List<Student>(students.OrderByDescending(x => x.Note).ToList());
+        Choosen = StudentsOrdered[0];
+
+        if (Choosen.Note < 8.0)
+        {
+            Console.WriteLine("Minimum note not reached");
+        }
+
+        else
+        {
+            Console.WriteLine(Choosen.Id.ToString());
+        }
+
+
+    }
+
+
+    public class Student
+    {
+        public int Id { get; set; }
+        public double Note { get; set; }
+
+        public Student(int id, double note)
+        {
+            Id = id;
+            Note = note;
+
+        }
+    }
+
+}
