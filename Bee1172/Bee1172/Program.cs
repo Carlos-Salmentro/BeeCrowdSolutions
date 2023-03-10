8bccf18d8dd0085eee623dfa3c3469a69f276358
CarlosSalmentro <carlos.salmentro@gmail.com>
2023-03-10 13:47:54 -0300

    Add - Bee1172

---
+++ Bee1172/Bee1172/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int[] Numbers = new int[10];
+
+        for (int i = 0; i < 10; i++)
+        {
+            int aux = int.Parse(Console.ReadLine());
+
+            if (aux <= 0)
+            {
+                Numbers[i] = 1;
+            }
+
+            else
+            {
+                Numbers[i] = aux;
+            }
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("X[" + i.ToString() + "] = " + Numbers[i]);
+        }
+
+    }
+
+}
