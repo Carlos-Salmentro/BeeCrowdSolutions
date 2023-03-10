520f4d0d666befee0896eeb32846cebef51d05b9
CarlosSalmentro <carlos.salmentro@gmail.com>
2023-03-09 23:33:59 -0300

    Add - Bee2029

---
+++ Bee2029/Bee2029/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        double Pi = 3.14;
+
+        while (true)
+        {
+            string vol = Console.ReadLine();
+            if (string.IsNullOrEmpty(vol))
+            {
+                break;
+            }
+
+            else
+            {
+                double Vol = Convert.ToDouble(vol, CultureInfo.InvariantCulture);
+                double d = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                double H = Vol / (Pi * Math.Pow((d / 2.0), 2.00));
+                double Area = Pi * Math.Pow((d / 2.0), 2.00);
+
+                Console.WriteLine("ALTURA = " + H.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("AREA = " + Area.ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
+
+    }
+}
