a6e6ea0180583942a33f676139d095d8c93677e5
CarlosSalmentro <carlos.salmentro@gmail.com>
2023-03-15 21:12:51 -0300

    Add -Bee1175

---
+++ Bee1175/Bee1175/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+
+        int[] N = new int[20];
+
+        for (int i = 0; i < N.Length; i++)
+        {
+            int Y = int.Parse(Console.ReadLine());
+            N[i] = Y;
+        }
+
+        for (int i = 0; i < N.Length / 2; i++)
+        {
+            int aux = N[i];
+            N[i] = N[N.Length - 1 - i];
+
+            N[N.Length - 1 - i] = aux;
+        }
+
+        for (int i = 0; i < N.Length; i++)
+        {
+            Console.WriteLine("N[" + i + "] = " + N[i]);
+        }
+
+
+    }
+
+}
