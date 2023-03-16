14339c10471500c35930c4d91a10d438aa38a5db
CarlosSalmentro <carlos.salmentro@gmail.com>
2023-03-16 19:31:09 -0300

    Add - Bee1176

---
+++ Bee1176/Bee1176/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        long[] Fib = new long[61];
+        Fib[0] = 0;
+        Fib[1] = 1;
+
+        for (int i = 2; i <= 60; i++)
+        {
+            Fib[i] = Fib[i - 2] + Fib[i - 1];
+        }
+
+        int T = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < T; i++)
+        {
+            int N = int.Parse(Console.ReadLine());
+            Console.WriteLine("Fib(" + N + ") = " + Fib[N]);
+        }
+
+
+    }
+
+}
