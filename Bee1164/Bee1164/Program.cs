bd33c0e13d73b83fcbc7e673f2d6dbeff7753f7e
CarlosSalmentro <carlos.salmentro@gmail.com>
2023-03-17 12:40:34 -0300

    Add - Bee1164

---
+++ Bee1164/Bee1164/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int N = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < N; i++)
+        {
+            int aux = 1;
+            int sum = 0;
+            int X = int.Parse(Console.ReadLine());
+
+            while (aux < X)
+            {
+                if (X % aux == 0.00)
+                {
+                    sum += aux;
+                }
+
+                aux++;
+            }
+
+            if (X == sum)
+            {
+                Console.WriteLine(X + " eh perfeito");
+            }
+
+            else
+            {
+                Console.WriteLine(X + " nao eh perfeito");
+            }
+        }
+
+    }
+
+}
