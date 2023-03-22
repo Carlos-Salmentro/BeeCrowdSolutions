82207e3c4611e6dc2265dd6eb313a15447dbdeda
CarlosSalmentro <carlos.salmentro@gmail.com>
2023-03-21 23:32:36 -0300

    Add - Bee1177

---
+++ Bee1177/Bee1177/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int[] Matriz = new int[1000];
+
+        int aux = 0;
+
+        int T = int.Parse(Console.ReadLine());
+
+        while (aux < 1000)
+        {
+            for (int i = 0; i < T && aux < 1000; i++)
+            {
+                Matriz[aux] = i;
+                aux++;
+            }
+        }
+
+        for (int i = 0; i < 1000; i++)
+        {
+            Console.WriteLine("N[" + i + "] = " + Matriz[i]);
+        }
+
+    }
+
+}
